8016f545a1c6e3d00e11acc2599e3e2c1b319627
Jonah Chates <jonah.chates@gmail.com>
2019-06-14 15:54:23 +0000

    Done.

--- tests.js
+++ tests.js
@@ -1,12 +1,10 @@
 var index = require("./index.js")
-
+  console.log(index.name)
 if (index.name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
-
-
 
 if (index.height === 70) {
     console.log("The height is correct")
